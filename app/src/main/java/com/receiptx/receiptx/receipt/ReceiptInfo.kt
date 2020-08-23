8628dc31a6883441176e1d6826e449e57adf892f
Mohammad Mujtaba Pirbhoy <mujtaba.pirbhoy@mail.utoronto.ca>
2020-08-22 22:50:14 -0700

    Added comment to rename receipt info to something more meaningful

--- app/src/main/java/com/receiptx/receiptx/receipt/ReceiptInfo.kt
+++ app/src/main/java/com/receiptx/receiptx/receipt/ReceiptInfo.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.receiptx.receiptx.R
 
+// TODO rename to something more meaningful e.g. ReceiptRow
 class ReceiptInfo : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
