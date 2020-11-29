330f671e76ab8d0e9665d41ab3dcc85c4b9dd34f
navie <naviechan@gmail.com>
2020-11-29 20:02:45 +0800

    Added item list in receipt

--- app/src/main/java/com/receiptx/receiptx/receipt/ReceiptFragment.kt
+++ app/src/main/java/com/receiptx/receiptx/receipt/ReceiptFragment.kt
@@ -1,9 +1,11 @@
 package com.receiptx.receiptx.receipt
 
 import android.os.Bundle
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.receiptx.receiptx.R
@@ -22,7 +24,8 @@ class ReceiptFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        dummy_receipt_holder.text = args.receiptArg.toString()
+        receipt_total_amount_holder.text = args.receiptArg.totalAmount.toString()
+        merchant_pic_holder.setImageResource(resources.getIdentifier("merchant_${args.receiptArg.merchantId}", "drawable", view.context.packageName))
 
     }
 
